9a4e1fcbbfc592e08ba3470adbae3239f1f6bbf4
ashamary97 <thomasmaryasha1997@gmail.com>
2020-11-14 01:51:03 +0530

    calculator updated stful

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'package:flutter/widgets.dart';
 
 void main()=>runApp(Home());
-class Home extends StatelessWidget {
-  TextEditingController firstnum=TextEditingController();
-  TextEditingController secondnum=TextEditingController();
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
 
+class _HomeState extends State<Home> {
+TextEditingController getnumone=TextEditingController();
+TextEditingController getnumtwo=TextEditingController();
+
+int result1=0;
+int result2=0;
+int result3=0;
+double result4=0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,75 +27,100 @@ class Home extends StatelessWidget {
         ),
         body: Container(
           padding: EdgeInsets.only(left: 20.0,right: 20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              TextField(
-                controller: firstnum,
-                decoration: InputDecoration(
-                  hintText: "Enter first number",
-                  border: OutlineInputBorder()
+          child: SingleChildScrollView(
+            child: Column(
+              //mainAxisAlignment: MainAxisAlignment.center,
+              //crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                TextField(
+                  controller: getnumone,
+                  decoration: InputDecoration(
+                    hintText: "Enter first number",
+                    border: OutlineInputBorder()
+                  ),
+
                 ),
-              ),
-              SizedBox(height: 20.0,),
+                SizedBox(height: 20.0,),
+                TextField(
+                  controller: getnumtwo,
+                  decoration: InputDecoration(
+                    hintText: "Enter second number",
+                    border: OutlineInputBorder()
+                  ),
 
-              TextField(
-                controller: secondnum,
-                decoration: InputDecoration(
-                  hintText: "Enter second number",
-                  border: OutlineInputBorder()
                 ),
-              ),
-              SizedBox(height: 10.0,),
-              RaisedButton(
-                color: Colors.blue,
-                onPressed: (){
+                SizedBox(height: 20.0,),
 
-                  int getnum1=int.parse(firstnum.text);
-                  int getnum2=int.parse(secondnum.text);
-                  int result=getnum1+getnum2;
-                  print(result);
+                RaisedButton(
+                  color: Colors.blue,
+                  onPressed: (){
+                    int numone=int.parse(getnumone.text);
+                    int numtwo=int.parse(getnumtwo.text);
 
-              },
-              child: Text("ADD"),
-              ),
-              SizedBox(height: 10.0,),
-              RaisedButton(
-                color: Colors.blue,
-                onPressed: (){
-                  int getnum1=int.parse(firstnum.text);
-                  int getnum2=int.parse(secondnum.text);
-                  int result=getnum1-getnum2;
-                  print(result);
+                    setState(() {
+                      result1=numone+numtwo;
 
-              },
-              child: Text("SUBTRACT"),
-              ),
-              SizedBox(height: 10.0,),
-              RaisedButton(
-                color: Colors.blue,
-                onPressed: (){
-                int getnum1=int.parse(firstnum.text);
-                int getnum2=int.parse(secondnum.text);
-                int result=getnum1*getnum2;
-                print(result);
-              },
-              child: Text("MULTIPLY"),),
-              SizedBox(height: 10.0,),
-              
-              RaisedButton(
-                color: Colors.blue,
-                onPressed: (){
-                int getnum1=int.parse(firstnum.text);
-                int getnum2=int.parse(secondnum.text);
-                double result=getnum1/getnum2;
+                    });
 
-                print(result);
-              },
-              child: Text("DIVIDE"),)
+                },
+                child: Text("ADD"),),
+                Text(result1.toString()),
 
-            ],
+                SizedBox(height: 20.0,),
+                RaisedButton(
+                  color: Colors.blue,
+                  onPressed: (){
+                    int num1=int.parse(getnumone.text);
+                    int num2=int.parse(getnumtwo.text);
+
+                    setState(() {
+                      result2=num1-num2;
+                    });
+
+                },
+                child: Text("SUBTRACT"),
+
+                ),
+                Text(result2.toString()),
+
+                SizedBox(height: 20.0,),
+                RaisedButton(
+                  color: Colors.blue,
+                  onPressed: (){
+                    int firstnum=int.parse(getnumone.text);
+                    int secondnum=int.parse(getnumtwo.text);
+
+                    setState(() {
+
+                      result3 = firstnum*secondnum;
+                    });
+
+                  },
+
+                child: Text("MULTIPLY"),),
+                Text(result3.toString()),
+
+                SizedBox(height: 20.0,),
+
+                RaisedButton(
+                 color: Colors.blue,
+                  onPressed: (){
+                    int first=int.parse(getnumone.text);
+                    int second=int.parse(getnumtwo.text);
+
+                    setState(() {
+
+                     result4 = first/second;
+                    });
+
+                  },
+
+                child: Text("DIVIDE"),),
+
+                Text(result4.toString()),
+
+              ],
+            ),
           ),
         ),
       ),
